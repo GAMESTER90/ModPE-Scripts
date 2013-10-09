744f6ada871c3bce0f14ec10bf4a28eaa0ca1c67
GAMESTER <tehgamester@gmail.com>
2013-10-09 13:04:19 -0500

    Update DiamondSword.js
    makes sure that you don't waste a sword.

--- DiamondSword.js
+++ DiamondSword.js
@@ -3,6 +3,12 @@ function useItem(x, y, z, itemId, blockId, side)
 {
 if (itemId==276)
 {
+  preventDefault();
 clientMessage("You have " + item);
 }
+else if (blockId = 1)
+{
+  preventDefault();
+  clientMessage("you hit a stone with a sword? you'll damage it!!!") }
 }
+//Sex and drugs, subminal message >;)
